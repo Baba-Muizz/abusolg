632c0140fc7ae2be37af171c553c15e09800954f
Yusuf AbdulMuizz <yusufabdulmuizz00@gmail.com>
2021-01-26 19:18:36 +0100

    contact section

--- layout/scripts/script.js
+++ layout/scripts/script.js
@@ -8,4 +8,24 @@ function myFunction(id) {
   }
 
 
+ // contact form validation
+function validateForm() {
+  var name =document.getElementById('name').value;
+  var message=document.getElementById('message').value;
+  var email=document.getElementById('email').value;
+  var phone=document.getElementById('phone').value;
+  var answer=document.getElementById('Answer');
   
+  if (name =="" || message=="" || email=="" || phone=="") {
+      answer.innerHTML="Fill All fields";
+      return false;
+  }
+  
+}
+// to input only text in name
+function validateName(event) {
+  char=event.which;
+  if(char >31 && char !=32 && (char <65 || char >90) && (char <97 || char >122)) {
+      return false;
+  }
+}
